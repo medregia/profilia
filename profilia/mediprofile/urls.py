b4a4500b09ca23e504dadef95f971f52686bb7ee
maishaq12 <medicheckia@gmail.com>
2024-12-20 00:28:27 +0300

    edit base successfully updated

--- profilia/mediprofile/urls.py
+++ profilia/mediprofile/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from mediprofile import views
 
 urlpatterns = [
-    path('kk/', views.display, name="index" ),
+    path('kk/', views.display, name="index"),
     path('ss/', views.addprofile, name="addprofile"),
-    path('delete/<id>', views.deleteview, name="deleteview" ),
+    path('delete/<int:id>/', views.deleteview, name="deleteview"),
+    path('edit/<int:id>/', views.editview, name="editview"),
 ]
+
--- profilia/mediprofile/views.py
+++ profilia/mediprofile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,  get_object_or_404
 from django.http import HttpResponse
 from mediprofile.models import Section1
 from mediprofile.forms import ProfileForm
@@ -21,3 +21,13 @@ def deleteview(request, id):
     form.delete()
     return redirect('/kk')
     
+def editview(request, id):
+    edit = Section1.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=edit)
+        if form.is_valid():
+            form.save()
+            return redirect('/kk')
+        else:
+             return HttpResponse( "hi")
+    return render(request, 'edit.html', {'edit':edit})
