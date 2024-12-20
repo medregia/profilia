19e84711d1024aff6745ac0629b099c302d2b352
maishaq12 <medicheckia@gmail.com>
2024-12-20 22:17:31 +0300

    small arrangement of design

--- profilia/mediprofile/urls.py
+++ profilia/mediprofile/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from mediprofile import views
 
 urlpatterns = [
-    path('kk/', views.display, name="index"),
-    path('ss/', views.addprofile, name="addprofile"),
+    path('home/', views.display, name="index"),
+    path('add/', views.addprofile, name="addprofile"),
     path('delete/<int:id>/', views.deleteview, name="deleteview"),
     path('edit/<int:id>/', views.editview, name="editview"),
 ]
